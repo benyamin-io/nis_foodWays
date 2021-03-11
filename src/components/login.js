757c883fbde21654fa5b6ed1f03f2583a6da13e3
benyamin-io <benyaminsembiring144@gmail.com>
2021-03-11 13:58:42 +0700

    add localstorage

--- src/components/login.js
+++ src/components/login.js
@@ -4,18 +4,42 @@ import {Link} from 'react-router-dom'
 
 export default function Login({handleCloseModal, showModal}) {
   
+  const [data, setData] = useState({email: '', password: ''})
+
+  const handleChange = e => {
+    setData({...data, [e.target.name]: e.target.value})
+  }
+
+  const handleLogin = e => {
+    e.preventDefault()
+    if(typeof(localStorage) !== 'undefined'){
+      localStorage.setItem("email", data.email)
+      localStorage.setItem("password", data.password)
+
+      console.log(localStorage.getItem('email'))
+      console.log(localStorage.getItem('password'))
+    }else{
+
+    }
+  }
+
   return (
     <>
       <Modal show={showModal} onHide={() => handleCloseModal('login')} centered dialogClassName="login-modalw">
         <div>
             <h3 className="mb-5">Login</h3>
-            <input type="text" name="email" placeholder="Email" className="p-2 w-100 mb-4" />
-            <input type="password" name="password" placeholder="password" className="p-2 w-100 mb-5" />
+            
+            <form onSubmit={handleLogin}>
+              <input type="text" name="email" placeholder="Email" className="p-2 w-100 mb-4" value={data.email} onChange={handleChange} />
 
-            <Button className="w-100">Login</Button>
-            <div className="text-center mt-3">
-              <Link to="">Don't have an account ? Click <b>Here</b></Link>
-            </div>
+              <input type="password" name="password" placeholder="password" className="p-2 w-100 mb-5" value={data.password} onChange={handleChange} />
+
+              <Button className="w-100" onClick={handleLogin}>Login</Button>
+              <div className="text-center mt-3">
+                <Link to="">Don't have an account ? Click <b>Here</b></Link>
+              </div>
+            </form>
+
         </div>
       </Modal>
     </>
